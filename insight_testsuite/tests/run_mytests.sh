9eff79d3e26b93f101cb72ebd0fb4285ba77c965
Pranay Rao <pranay.r.r@gmail.com>
2016-04-04 06:34:33 -0400

    Delete run_mytests.sh

--- insight_testsuite/tests/run_mytests.sh
+++
@@ -1,7 +0,0 @@
-python ./src/average_degree.py './insight_testsuite/tests/test_1/tweet_input/tweets.txt' './insight_testsuite/tests/test_1/tweet_output/output.txt'
-python ./src/average_degree.py './insight_testsuite/tests/test_2/tweet_input/tweets.txt' './insight_testsuite/tests/test_2/tweet_output/output.txt'
-python ./src/average_degree.py './insight_testsuite/tests/test-2-tweets-all-distinct/tweet_input/tweets.txt' './insight_testsuite/tests/test-2-tweets-all-distinct/tweet_output/output.txt'
-
-cmp insight_testsuite/tests/test_1/tweet_output/output.txt insight_testsuite/tests/test_1/tweet_output/expected_output.txt && echo "The actual output matches the expected output for test_1" || echo "The actual output does not match the expected output for test_1" 
-cmp insight_testsuite/tests/test_2/tweet_output/output.txt insight_testsuite/tests/test_2/tweet_output/expected_output.txt && echo "The actual output matches the expected output for test_2" || echo "The actual output does not match the expected output for test_2"
-cmp insight_testsuite/tests/test-2-tweets-all-distinct/tweet_output/output.txt insight_testsuite/tests/test-2-tweets-all-distinct/tweet_output/expected_output.txt && echo "The actual output matches the expected output for test-2-tweets-all-distinct" || echo "The actual output does not match the expected output for test-2-tweets-all-distinct" 
